9e94af024c2cb4e53ec947314623aade121939d0
antony-moreira <tonycircle00@gmail.com>
2026-04-12 19:48:04 -0300

    update disks

--- backups/scripts/mount-disk.sh
+++ backups/scripts/mount-disk.sh
@@ -2,8 +2,8 @@
 #
 #monta disco no /mnt
 sudo chown -R ubuntu:ubuntu /mnt/*
-sudo mount -t ext4 /dev/sdb /mnt/homelab-ssd-thor/
-sudo mount -t ext4 /dev/sdc /mnt/homelab-hd-externo-thor/
+sudo mount -t ext4 /dev/sdb /mnt/homelab-hd-interno/
+sudo mount -t ext4 /dev/sdc /mnt/homelab-hd-externo/
 
 #inicia docker depois de montar o disco
 sudo systemctl start docker
