5c3598ab7208c11bb0cd3bc62e6ab236738b7929
antony-moreira <tonycircle00@gmail.com>
2026-04-19 19:16:26 -0300

    fix paths

--- backups/scripts/mount-disk.sh
+++ backups/scripts/mount-disk.sh
@@ -2,8 +2,7 @@
 #
 #monta disco no /mnt
 sudo chown -R antony:antony /mnt
-sudo mount -t ext4 /dev/sdb /mnt/homelab-hd-filmes/
-sudo mount -t ext4 /dev/sdc /mnt/homelab-hd-series/
+sudo mount -t ext4 /dev/sdb /mnt/homelab-hd-externo/
 
 #inicia docker depois de montar o disco
 sudo systemctl start docker
